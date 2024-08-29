04765c9ec9f468a4272ceeb714cb7b019001777a
Matthias Fauconneau <matthias.fauconneau@huawei.com>
2024-08-29 11:22:50 +0200

    -am types

--- vulkano-shaders/src/lib.rs
+++ vulkano-shaders/src/lib.rs
@@ -6,20 +6,29 @@ use proc_macro::{TokenTree, TokenStream, Ident, Literal, Span, quote};
 #[fehler::throws(Box<dyn std::error::Error>)] fn shader_proc(MacroInput(name): MacroInput) -> TokenStream {
 	let spirv = vulkano::shader::spirv::Spirv::new(bytemuck::cast_slice(&std::fs::read(&std::path::Path::new(&std::env::var("OUT_DIR")?).join(name.to_string()+".spv"))?))?;
 	TokenStream::from_iter(spirv.types().iter().filter_map(|spirv_struct| {
-		use vulkano::shader::spirv::Instruction;
-		let Instruction::TypeStruct{result_id: id, member_types} = spirv_struct else { return None; };
-		let name_from_id = |id| spirv.id(id).names().iter().find_map(|instruction| if let Instruction::Name{name, .. } = instruction { Some(name) } else { None });
+		use vulkano::shader::spirv::Instruction::*;
+		let TypeStruct{result_id: id, member_types} = spirv_struct else { return None; };
+		let name_from_id = |id| spirv.id(id).names().iter().find_map(|instruction| if let Name{name, .. } = instruction { Some(name) } else { None });
 		let name = name_from_id(*id)?;
-		if name != "Uniforms" && name != "RuntimeArrayItemType" { return None; }
 		let members = TokenStream::from_iter(member_types.iter().zip(spirv.id(*id).members()).map(|(&id, field)| {
-			let member_name = field.names().iter().find_map(|instruction| if let Instruction::MemberName { name, .. } = instruction { Some(TokenTree::Ident(Ident::new(name, Span::call_site()))) } else { None }).expect("name");
+			let member_name = field.names().iter().find_map(|instruction| if let MemberName { name, .. } = instruction { Some(TokenTree::Ident(Ident::new(name, Span::call_site()))) } else { None }).expect("name");
 			match spirv.id(id).instruction() {
-				Instruction::TypeFloat{..} => quote!(pub $member_name: f32,),
-				Instruction::TypeVector{component_count, ..} => {
-					let component_count = TokenTree::Literal(Literal::usize_unsuffixed(*component_count as usize));
-					quote!(pub $member_name: [f32; $component_count],)
+				TypeFloat{..} => quote!(pub $member_name: half::f16,),
+				TypeInt{..} => quote!(pub $member_name: u32,),
+				&TypeVector{component_count, component_type, ..} => {
+					let component_count = TokenTree::Literal(Literal::usize_unsuffixed(component_count as usize));
+					let TypeFloat{width, ..} = spirv.id(component_type).instruction() else {panic!("{component_type:?}")};
+					match width {
+						16 => quote!(pub $member_name: [half::f16; $component_count],),
+						32 => quote!(pub $member_name: [f32; $component_count],),
+						_ => unimplemented!()
+					}
 				},
-				Instruction::TypeStruct{..} => {
+				TypeMatrix{column_count, ..} => {
+					let column_count = TokenTree::Literal(Literal::usize_unsuffixed(*column_count as usize));
+					quote!(pub $member_name: [[f32; $column_count]; $column_count],)
+				},
+				TypeStruct{..} => {
 					let ty = TokenTree::Ident(Ident::new(name_from_id(id).unwrap(), Span::call_site()));
 					quote!(pub $member_name: $ty,)
 				}
@@ -27,6 +36,6 @@ use proc_macro::{TokenTree, TokenStream, Ident, Literal, Span, quote};
 			}
 		}));
 		let name = TokenTree::Ident(Ident::new(name, Span::call_site()));
-		Some(quote!{#[repr(C)]#[derive(vulkano::buffer::subbuffer::BufferContents,Clone,Copy)] pub struct $name { $members }})
+		Some(quote!{#[repr(C)]#[derive(Clone,Copy,bytemuck::Zeroable,bytemuck::Pod)] pub struct $name { $members }})
 	}))
 }
